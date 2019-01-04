217d6722919b01ae52bbe8309c2eaa94d95d1d8b
Augustine Kwanashie <akwanashie@gmail.com>
2019-01-04 17:49:29 +0000

    Add linter

--- src/general/decorators.ts
+++ src/general/decorators.ts
@@ -1,10 +1,23 @@
-function log (target: Function) {
-  console.log('Logging the random class')
+namespace decorators {
+  type Constructor<T = {}> = new (...args: any[]) => T;
+
+  function author (name: string) {
+    return function (Target: Constructor) {
+      console.log(`written by ${name}`)
+      return class extends Target {
+        authorName () { return name }
+      }
+    }
+  }
+
+  function log (target: Function) {
+    console.log(`Logging ${target.name} class`)
+  }
+
+  @author('Augustine Kwanashie')
+  @log
+  class Random { }
+
+  const r: any = new Random()
+  console.log(r.authorName())
 }
-
-@log
-class Random {
-
-}
-
-new Random()
